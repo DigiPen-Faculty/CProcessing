481914d0234dae97a737d27ed0c47359dca3cc6a
Gerald Wong <email@geraldwong.net>
2022-08-31 13:11:20 +0800

    Fixed CreateLibraryPackageSG.bat. Updated SG's release version of CProcessing package.

--- Processing_Sample/CreateLibraryPackageSG.bat
+++ Processing_Sample/CreateLibraryPackageSG.bat
@@ -13,37 +13,37 @@ rem - We assume that users have already installed the required VS2019, to the po
 rem TODO
 
 
-rem --- CProcessingPackage Phase ---
-rmdir .\CProcessingPackage /s /q
-mkdir .\CProcessingPackage
-mkdir .\CProcessingPackage\CProcessing
-mkdir .\CProcessingPackage\CProcessing\inc
-mkdir .\CProcessingPackage\CProcessing\lib
-mkdir .\CProcessingPackage\CProcessing\lib\x64
-mkdir .\CProcessingPackage\CProcessing\lib\x86
+rem --- CProcessingPackageSG Phase ---
+rmdir .\CProcessingPackageSG /s /q
+mkdir .\CProcessingPackageSG
+mkdir .\CProcessingPackageSG\CProcessing
+mkdir .\CProcessingPackageSG\CProcessing\inc
+mkdir .\CProcessingPackageSG\CProcessing\lib
+mkdir .\CProcessingPackageSG\CProcessing\lib\x64
+mkdir .\CProcessingPackageSG\CProcessing\lib\x86
 
 rem x64
-xcopy .\CProcessing\lib\x64\CProcessing.lib .\CProcessingPackage\CProcessing\lib\x64 /s /r /y /q
-xcopy .\CProcessing\lib\x64\CProcessing.dll .\CProcessingPackage\CProcessing\lib\x64 /s /r /y /q
-xcopy .\CProcessing\lib\x64\CProcessingd.lib .\CProcessingPackage\CProcessing\lib\x64 /s /r /y /q
-xcopy .\CProcessing\lib\x64\CProcessingd.dll .\CProcessingPackage\CProcessing\lib\x64 /s /r /y /q
-xcopy .\CProcessing\lib\x64\fmod.dll .\CProcessingPackage\CProcessing\lib\x64 /s /r /y /q
-xcopy .\CProcessing\lib\x64\fmodL.dll .\CProcessingPackage\CProcessing\lib\x64 /s /r /y /q
+xcopy .\CProcessing\lib\x64\CProcessing.lib .\CProcessingPackageSG\CProcessing\lib\x64 /s /r /y /q
+xcopy .\CProcessing\lib\x64\CProcessing.dll .\CProcessingPackageSG\CProcessing\lib\x64 /s /r /y /q
+xcopy .\CProcessing\lib\x64\CProcessingd.lib .\CProcessingPackageSG\CProcessing\lib\x64 /s /r /y /q
+xcopy .\CProcessing\lib\x64\CProcessingd.dll .\CProcessingPackageSG\CProcessing\lib\x64 /s /r /y /q
+xcopy .\CProcessing\lib\x64\fmod.dll .\CProcessingPackageSG\CProcessing\lib\x64 /s /r /y /q
+xcopy .\CProcessing\lib\x64\fmodL.dll .\CProcessingPackageSG\CProcessing\lib\x64 /s /r /y /q
 
 rem x86
-xcopy .\CProcessing\lib\Win32\CProcessing.lib .\CProcessingPackage\CProcessing\lib\x86 /s /r /y /q
-xcopy .\CProcessing\lib\Win32\CProcessing.dll .\CProcessingPackage\CProcessing\lib\x86 /s /r /y /q
-xcopy .\CProcessing\lib\Win32\CProcessingd.lib .\CProcessingPackage\CProcessing\lib\x86 /s /r /y /q
-xcopy .\CProcessing\lib\Win32\CProcessingd.dll .\CProcessingPackage\CProcessing\lib\x86 /s /r /y /q
-xcopy .\CProcessing\lib\Win32\fmod.dll .\CProcessingPackage\CProcessing\lib\x86 /s /r /y /q
-xcopy .\CProcessing\lib\Win32\fmodL.dll .\CProcessingPackage\CProcessing\lib\x86 /s /r /y /q
+xcopy .\CProcessing\lib\Win32\CProcessing.lib .\CProcessingPackageSG\CProcessing\lib\x86 /s /r /y /q
+xcopy .\CProcessing\lib\Win32\CProcessing.dll .\CProcessingPackageSG\CProcessing\lib\x86 /s /r /y /q
+xcopy .\CProcessing\lib\Win32\CProcessingd.lib .\CProcessingPackageSG\CProcessing\lib\x86 /s /r /y /q
+xcopy .\CProcessing\lib\Win32\CProcessingd.dll .\CProcessingPackageSG\CProcessing\lib\x86 /s /r /y /q
+xcopy .\CProcessing\lib\Win32\fmod.dll .\CProcessingPackageSG\CProcessing\lib\x86 /s /r /y /q
+xcopy .\CProcessing\lib\Win32\fmodL.dll .\CProcessingPackageSG\CProcessing\lib\x86 /s /r /y /q
 
 rem inc
-xcopy .\CProcessing\inc\cprocessing.h .\CProcessingPackage\CProcessing\inc /s /r /y /q
-xcopy .\CProcessing\inc\cprocessing_common.h .\CProcessingPackage\CProcessing\inc /s /r /y /q
+xcopy .\CProcessing\inc\cprocessing.h .\CProcessingPackageSG\CProcessing\inc /s /r /y /q
+xcopy .\CProcessing\inc\cprocessing_common.h .\CProcessingPackageSG\CProcessing\inc /s /r /y /q
 
 rem --- Zipping phase --- 
 rem I hope you are on Powershell v5 or above...
-pushd .\CProcessingPackage
+pushd .\CProcessingPackageSG
 powershell Compress-Archive -LiteralPath "CProcessing" -DestinationPath "CProcessingSG.zip"
 popd
